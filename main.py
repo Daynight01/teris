20f45a47e242dc8ec61fc62287986d8847214c67
ShadyGames329 <164423847+ShadyGames329@users.noreply.github.com>
2025-03-25 14:27:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,29 @@ pygame.init()
 
 
 SCREEN_HEIGHT=900
-SCREEN_WIDTH=700
+SCREEN_WIDTH=900
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 run = True
 pygame.display.set_caption("Tetris")
 clock = pygame.time.Clock()
-x=0
-y=0
 
 def grid():
-    for i in range(5):
-        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect((x, y, 50, 50)))
-        x+=1
-        y+=1
+    for x in range(10):
+        pygame.draw.rect(screen, (200, 200, 200), pygame.Rect((x*45 + 225, 0, 45, 45)), 2)
+        
+        for y in range(20):
+            pygame.draw.rect(screen, (200, 200, 200), pygame.Rect((x*45 + 225, y*45 + 45, 45, 45)), 2)
+
+    pygame.display.update()
+
+grid()
 
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    grid()
+    #Updates
     pygame.display.update()
     clock.tick(60)
