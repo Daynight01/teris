90f8fdf13004b2a978836aafcc607c31944b6419
Daynight01 <136109693+Daynight01@users.noreply.github.com>
2025-03-25 13:49:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import time, random, os, math, pygame
+pygame.init()
+
+SCREEN_HEIGHT=900
+SCREEN_WIDTH=700
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+run = True
+
+while run:
+    
+
+    pygame.display.update()
